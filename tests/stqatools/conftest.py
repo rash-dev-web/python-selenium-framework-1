fb63bcaef765d12d9b7b91e92fbc93d170ab1a52
Rasheed Mohammad <rasheed.m@live.com>
2021-01-27 23:26:40 +0530

    added the code for command line parameter, now tests can be run on firefox as well

--- tests/stqatools/conftest.py
+++ tests/stqatools/conftest.py
@@ -2,11 +2,23 @@ import pytest
 from selenium import webdriver
 
 URL = "https://stqatools.com/demo/index.php"
+driver = None
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser-name", action="store", default="chrome"
+    )
 
 
 @pytest.fixture()
 def setup(request):
-    driver = webdriver.Chrome(executable_path="../../drivers/chromedriver.exe")
+    browser_name = request.config.getoption("--browser-name")
+    global driver
+    if browser_name == "chrome":
+        driver = webdriver.Chrome(executable_path="../../drivers/chromedriver.exe")
+    elif browser_name == "firefox":
+        driver = webdriver.Firefox(executable_path="../../drivers/geckodriver.exe")
     driver.maximize_window()
     driver.get(URL)
     request.cls.driver = driver
